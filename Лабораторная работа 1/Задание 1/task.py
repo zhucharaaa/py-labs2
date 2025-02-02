362dfda31b9e399ac9ae7ce05b25982761ca70ce
zhucharaaa <zhukovaaa3781@gmail.com>
2025-02-02 20:39:15 +0300

    Update task.py

--- Лабораторная работа 1/Задание 1/task.py
+++ Лабораторная работа 1/Задание 1/task.py
@@ -27,15 +27,15 @@ class Smartphone:
 
     def smartphone_increase_memory(self, sd_memory: int) -> None:
         """
-               Увеличение памяти.
-               :param sd_memory: Добавление памяти через использование sd карты
+        Увеличение памяти.
+        :param sd_memory: Добавление памяти через использование sd карты
 
-               :raise ValueError: Вызываем ошибку, если отсутствует слот под sd-карту
+        :raise ValueError: Вызываем ошибку, если отсутствует слот под sd-карту
 
-               Примеры:
-               >>> iPhone = Smartphone(128, 2000)
-               >>> iPhone.smartphone_increase_memory(32)
-               """
+        Примеры:
+        >>> iPhone = Smartphone(128, 2000)
+        >>> iPhone.smartphone_increase_memory(32)
+        """
         if not isinstance(sd_memory, int):
             raise TypeError("Объем sd-карты должен быть целым числом")
         if sd_memory < 0:
@@ -44,18 +44,22 @@ class Smartphone:
 
     def is_smartphone_nfc(self) -> bool:
         """
-               Функция проверки наличия NFC в смартфоне.
-               :return: Есть ли NFC в смартфоне
+        Функция проверки наличия NFC в смартфоне.
+        :return: Есть ли NFC в смартфоне
 
-               Примеры:
-               >>> iPhone = Smartphone(128, 2000)
-               >>> iPhone.is_smartphone_nfc()
-               """
+        Примеры:
+        >>> iPhone = Smartphone(128, 2000)
+        >>> iPhone.is_smartphone_nfc()
+        """
         ...
 
 
 class Car:
-    def __init__(self, weightlifters_weight: Union[int, float], weightlifter_height: Union[int, float]):
+    def __init__(
+        self,
+        weightlifters_weight: Union[int, float],
+        weightlifter_height: Union[int, float],
+    ):
         """
         Создание и подготовка к работе объекта "Машина"
 
@@ -79,15 +83,15 @@ class Car:
 
     def car_wins(self, number_wins: Union[int, float]) -> None:
         """
-               Перезарядка оружия.
-               :param number_wins: Количество завоеванных побед
+        Перезарядка оружия.
+        :param number_wins: Количество завоеванных побед
 
-               :raise ValueError: Вызываем ошибку, если количество побед не является числом
+        :raise ValueError: Вызываем ошибку, если количество побед не является числом
 
-               Примеры:
-               >>> Schwarzenegger = Car(110, 185)
-               >>> Schwarzenegger.car_wins(7)
-               """
+        Примеры:
+        >>> Schwarzenegger = Car(110, 185)
+        >>> Schwarzenegger.car_wins(7)
+        """
         if not isinstance(number_wins, (int, float)):
             raise TypeError("Количество побед должно быть числом")
         if number_wins < 0:
@@ -96,15 +100,15 @@ class Car:
 
     def weight_bench(self, number_kg: Union[int, float]) -> None:
         """
-               Перезарядка оружия.
-               :param number_kg: Вес штанги на жиме лежа
+        Перезарядка оружия.
+        :param number_kg: Вес штанги на жиме лежа
 
-               :raise ValueError: Вызываем ошибку, если Вес штанги не является числом
+        :raise ValueError: Вызываем ошибку, если Вес штанги не является числом
 
-               Примеры:
-               >>> Schwarzenegger = Car(110, 185)
-               >>> Schwarzenegger.weight_bench(227)
-               """
+        Примеры:
+        >>> Schwarzenegger = Car(110, 185)
+        >>> Schwarzenegger.weight_bench(227)
+        """
         if not isinstance(number_kg, (int, float)):
             raise TypeError("Вес штанги должен быть числом")
         if number_kg < 0:
@@ -113,13 +117,13 @@ class Car:
 
     def is_car_use_doping(self) -> bool:
         """
-               Функция которая проверяет использовал ли штангист допинг.
-               :return: использует ли штангист допинг
+        Функция которая проверяет использовал ли штангист допинг.
+        :return: использует ли штангист допинг
 
-               Примеры:
-               >>> Schwarzenegger = Car(110, 185)
-               >>> Schwarzenegger.is_car_use_doping()
-               """
+        Примеры:
+        >>> Schwarzenegger = Car(110, 185)
+        >>> Schwarzenegger.is_car_use_doping()
+        """
         ...
 
 
@@ -148,23 +152,24 @@ class Arthropods:
 
     def arthropods_danger(self) -> bool:
         """
-               Функция проверки наличия яда у членистоногого.
-               :return: Есть ли яд у членистоного
+        Функция проверки наличия яда у членистоногого.
+        :return: Есть ли яд у членистоного
 
-               Примеры:
-               >>> spider = Arthropods(8, 6)
-               >>> spider.arthropods_danger()
-               """
+        Примеры:
+        >>> spider = Arthropods(8, 6)
+        >>> spider.arthropods_danger()
+        """
 
     def arthropods_claws(self) -> bool:
         """
-               Функция проверки наличия клешней у членистоногого.
-               :return: Есть ли клешни у членистоного
+        Функция проверки наличия клешней у членистоногого.
+        :return: Есть ли клешни у членистоного
 
-               Примеры:
-               >>> spider = Arthropods(8, 6)
-               >>> spider.arthropods_claws()
-               """
+        Примеры:
+        >>> spider = Arthropods(8, 6)
+        >>> spider.arthropods_claws()
+        """
+
 
 if __name__ == "__main__":
     doctest.testmod()  # Проводим проверку примеров, приведенных в документации

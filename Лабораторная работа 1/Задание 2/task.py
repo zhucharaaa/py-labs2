5577e3fe0fe4b7fb9f22110361ea05323e3105f0
zhucharaaa <zhukovaaa3781@gmail.com>
2025-02-02 20:56:38 +0300

    Update task.py

--- Лабораторная работа 1/Задание 2/task.py
+++ Лабораторная работа 1/Задание 2/task.py
@@ -1,20 +1,20 @@
 from task_1 import Smartphone, Car, Arthropods
 
 if __name__ == "__main__":
- # TODO: инстанцировать все описанные классы, создав три объекта.C()
+    # TODO: инстанцировать все описанные классы, создав три объекта.C()
 
     try:
-     smartphone1 = Smartphone (256 , 3000)
-     smartphone1.smartphone_increase_memory(-32)
+        smartphone1 = Smartphone(256, 3000)
+        smartphone1.smartphone_increase_memory(-32)
     except ValueError:
-        print('Ошибка: неправильные данные')
+        print("Ошибка: неправильные данные")
 
     try:
-     car1 = Car('may', 210)
+        car1 = Car("may", 210)
     except TypeError:
-        print('Ошибка: неправильные данные')
+        print("Ошибка: неправильные данные")
 
     try:
-     arthropods1 = Arthropods (24 , '-5' )
+        arthropods1 = Arthropods(24, "-5")
     except TypeError:
-        print('Ошибка: неправильные данные')
+        print("Ошибка: неправильные данные")
